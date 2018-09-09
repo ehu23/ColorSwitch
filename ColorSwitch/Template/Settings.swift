8f8e8f45540f8ac668c5c71dc0db8c0852b0d338
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-09 10:44:03 -0700

    ball contact physics write

---
+++ ColorSwitch/Template/Settings.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  ColorSwitch
+//
+//  Created by Edward Hu on 9/9/18.
+//  Copyright © 2018 Edward Hu. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1 //       1, which is 01 in binary
+    static let switchCategory: UInt32 = 0x1 << 1 //2, which is 10 in binary
+}
