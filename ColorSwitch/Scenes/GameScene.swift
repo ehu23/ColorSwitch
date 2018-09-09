0b2f04a190f77cf774e387f87c1b6338c74c6f80
Eddie Hu <ehu23@users.noreply.github.com>
2018-09-09 10:26:42 -0700

    Basic asset and static design complete

--- ColorSwitch/Scenes/GameScene.swift
+++ ColorSwitch/Scenes/GameScene.swift
@@ -11,10 +11,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var colorSwitch: SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        
+        layoutScene()
     }
     
+    func layoutScene() {
+        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+        
+        colorSwitch = SKSpriteNode(imageNamed: "ColorCircle")
+        colorSwitch.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
+        colorSwitch.position = CGPoint(x: frame.midX, y: frame.minY + colorSwitch.size.height)
+        
+        addChild(colorSwitch)
+        
+        spawnBall()
+    }
     
+    func spawnBall() {
+        let ball = SKSpriteNode(imageNamed: "Ball")
+        ball.size = CGSize(width: 30.0, height: 30.0)
+        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
+        addChild(ball)
+        
+        
+    }
 }
